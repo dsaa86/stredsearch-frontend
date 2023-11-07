ccd50800e898f460aa9c6284bd0cfa0b15a8ea78
Matthew Dunn <mattjohndunn@gmail.com>
2023-11-07 19:31:30 +0400

    Built out simple component list for a Stack Question

--- app/src/App.js
+++ app/src/App.js
@@ -1,37 +1,62 @@
 import {useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+
   return (
-    
+    <StackQuestionContainer />
   );
 }
-
-export default App;
 
 
 function StackQuestionContainer(){
 
+  const [questionTitle, setQuestionTitle] = useState("Title");
+  const [questionTags, setQuestionTags] = useState(["Tag"]);
+  const [questionUser, setQuestionUser] = useState("User");
+  const [questionAccepted, setQuestionAccepted] = useState("Accepted");
+  const [questionAnswerCount, setQuestionAnswerCount] = useState(0);
+  const [questionCreatedDate, setQuestionCreatedDate] = useState(0);
+  const [questionLastActivityDate, setQuestionLastActivityDate] = useState(0);
+  const [questionVoteNumber, setQuestionVoteNumber] = useState(0);
+
+  const question = {
+    title: questionTitle,
+    tagList: questionTags,
+    username: questionUser,
+    accepted: questionAccepted,
+    answerCount: questionAnswerCount,
+    createdDate: questionCreatedDate,
+    lastActivityDate: questionLastActivityDate,
+    voteNumber: questionVoteNumber
+  };
+
 
   return(
     <div className="stack-question-container">
-      <StackQuestionTitle />
-      <StackQuestionTags />
+      <StackQuestionTitle question={question}/>
+      <StackQuestionTags question={question}/>
+      <StackQuestionUser question={question}/>
+      <StackQuestionAccepted question={question}/>
+      <StackQuestionAnswerCount question={question}/>
+      <StackQuestionCreatedDate question={question}/>
+      <StackQuestionLastActivityDate question={question}/>
+      <StackQuestionVoteNumber question={question}/>
     </div>
   );
 }
 
-function StackQuestionTitle(){
+function StackQuestionTitle({question}){
   return(
     <div className="stack-question-title">
-      <span className="stack-question-title-value">{ questionTitleValue }</span>
+      <span>{ question.title }</span>
     </div>
   );
 }
 
-function StackQuestionTags(){
+function StackQuestionTags({question}){
 
-  const tags = tagList.map(tag =>
+  const tags = question.tagList.map(tag =>
     <li key={tag.id}>
       {tag.name}
     </li>
@@ -45,8 +70,54 @@ function StackQuestionTags(){
 }
 
 
+function StackQuestionUser({question}){
+  return(
+    <div className="stack-question-user">
+      <span>{question.username}</span>
+    </div>
+  );
+}
 
+function StackQuestionAccepted({question}){
+  return(
+    <div className="stack-question-accepted">
+      <span>{question.accepted}</span>
+    </div>
+  );
+}
 
+function StackQuestionAnswerCount({question}){
+  return(
+    <div className="stack-question-answer-count">
+      <span>{question.answerCount}</span>
+    </div>
+  );
+}
+
+function StackQuestionCreatedDate({question}){
+  return(
+    <div className="stack-question-created-date">
+      <span>{question.createdDate}</span>
+    </div>
+  );
+
+}
+
+function StackQuestionLastActivityDate({question}){
+  return(
+    <div className="stack-question-last-activity-date">
+      <span>{question.lastActivityDate}</span>
+    </div>
+  );
+}
+
+function StackQuestionVoteNumber({question}){
+  return(
+    <div className="stack-question-vote-number">
+      <span>{question.voteNumber}</span>
+    </div>
+  );
+}
 
 
 
