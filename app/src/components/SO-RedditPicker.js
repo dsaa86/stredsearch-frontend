82d64719caa3eb6da0a9be364837b8977b1163e4
Matthew Dunn <mattjohndunn@gmail.com>
2023-11-30 17:41:52 +0400

    Refactored Stack-Reddit Picker

--- app/src/components/SO-RedditPicker.js
+++ app/src/components/SO-RedditPicker.js
@@ -1,29 +1,28 @@
-import {useEffect, useState} from 'react';
-import Axios from 'axios';
-import './SearchAppComponentsStyle.css';
+import SoRedditPickerInput from "./SORedditPickerInput";
+import "./SearchAppComponentsStyle.css";
 
-export default function SoRedditPicker({showSO, showReddit, soCheckHandler, redditCheckHandler}){
-    return(
-        <div className="row">
-            <div className="container so-reddit-picker">
-                <div className="row">
-                    <div className="col-4 col-md-3 col-lg-2">
-                        <label for="so-checkbox">Stack Overflow</label>
-                    </div>
-                    <div className="col-8 col-md-9 col-lg-10">
-                        <input type="checkbox" id="so-checkbox" name="so-checkbox" checked={showSO} onChange={soCheckHandler}/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-4 col-md-3 col-lg-2">
-                        <label for="reddit-checkbox">Reddit</label>
-                    </div>
-                    <div className="col-8 col-md-9 col-lg-10">
-                        <input type="checkbox" id="reddit-checkbox" name="reddit-checkbox" checked={showReddit} onChange={redditCheckHandler}/>
-                    </div>
-                </div>
-                    
-            </div>
-        </div>
-    );
+export default function SoRedditPicker({
+	showSO,
+	showReddit,
+	soCheckHandler,
+	redditCheckHandler,
+}) {
+	return (
+		<div className="row">
+			<div className="container so-reddit-picker">
+				<SoRedditPickerInput
+					identifier={"so"}
+					formattedIdentifier={"Stack Overflow"}
+					checkedState={showSO}
+					changeHandler={soCheckHandler}
+				/>
+				<SoRedditPickerInput
+					identifier={"reddit"}
+					formattedIdentifier={"Reddit"}
+					checkedState={showReddit}
+					changeHandler={redditCheckHandler}
+				/>
+			</div>
+		</div>
+	);
 }
---
+++ app/src/components/SORedditPickerInput.js
@@ -0,0 +1,39 @@
+import "./SearchAppComponentsStyle.css";
+
+export default function SoRedditPickerInput({
+	identifier,
+	formattedIdentifier,
+	checkedState,
+	changeHandler,
+}) {
+	return (
+		<div
+			id={`${identifier}-checkbox-row`}
+			className="row"
+		>
+			<div
+				id={`${identifier}-checkbox-label-col`}
+				className="col-4 col-md-3 col-lg-2"
+			>
+				<label
+					id={`${identifier}-checkbox-label`}
+					htmlFor={`${identifier}-checkbox`}
+				>
+					{formattedIdentifier}
+				</label>
+			</div>
+			<div
+				id={`${identifier}-checkbox-row`}
+				className="col-8 col-md-9 col-lg-10"
+			>
+				<input
+					type="checkbox"
+					id={`${identifier}-checkbox`}
+					name={`${identifier}-checkbox`}
+					checked={checkedState}
+					onChange={changeHandler}
+				/>
+			</div>
+		</div>
+	);
+}
